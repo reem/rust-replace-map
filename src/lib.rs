3b6bfcffe3e5450bd778a3210d632a5e587e425c
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-02 20:31:07 -0800

    (fix) Mark replace-map as unsafe because it is exception unsafe.
    If the passed-in mapper panics, replace-map will cause destructors
    to run twice, which can be memory unsafe.
    
    [breaking-change]

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,20 @@ use std::ptr;
 /// Does not create an intermediate value, so is more efficient and
 /// ergonomic in cases where producing a value to pass to mem::replace
 /// is hard.
-pub fn replace_map<'a, T, F>(src: &mut T, prod: F)
+///
+/// The passed in closure *must* not panic or destructors will run on
+/// an instance of T twice.
+pub unsafe fn replace_map<'a, T, F>(src: &mut T, prod: F)
 where F: FnOnce(T) -> T {
     // Read the value, pass it to prod, then write-over src.
-    //
-    // Safe because the value originally behind src is dropped
-    // inside of prod and is then immediately written over.
-    unsafe { *src = prod(ptr::read(src as *mut T as *const T)); }
+    *src = prod(ptr::read(src as *mut T as *const T));
 }
 
 #[test] fn test_works() {
     let mut a = 7u;
     let b = &mut a;
 
-    replace_map(b, |: x: uint| x * 2);
+    unsafe { replace_map(b, |: x: uint| x * 2); }
     assert_eq!(*b, 14u);
 }
 
