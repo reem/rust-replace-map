202b37bcc65d02d26afc557c211511dc351e77fe
Jonathan Reem <jonathan.reem@gmail.com>
2015-02-10 21:01:14 -0800

    (fix) Mark unsafe again, due to discovered unsafety.

--- src/lib.rs
+++ src/lib.rs
@@ -15,21 +15,46 @@ use std::ptr;
 /// Does not create an intermediate value, so is more efficient and
 /// ergonomic in cases where producing a value to pass to mem::replace
 /// is hard.
-pub fn replace_map<'a, T, F>(src: &mut T, prod: F)
+///
+/// This is not a totally safe function, it has some curious edge cases.
+/// Generally you should only pass in a reference the data behind
+/// that reference owns itself. An example may be helpful:
+///
+/// Unsafe Usage:
+///
+/// ```ignore
+/// struct Foo { num: Box<u32> }
+/// struct Bar { foo: Foo }
+///
+/// impl Drop for Bar {
+///     fn drop(&mut self) {
+///         *self.foo.num
+///     }
+/// }
+///
+/// let mut b = Bar { foo: Foo { num: Box::new(123) } };
+///
+/// // replace_map will zero b.foo.num when it reads it and passes
+/// // it to the closure. The closure then panics, leaving b.foo.num
+/// // set to 0, which then causes a null-pointer dereference in b's
+/// // destructors.
+/// unsafe { replace_map(&mut b.foo.num, |_| panic!()); }
+/// ```
+pub unsafe fn replace_map<'a, T, F>(src: &mut T, prod: F)
 where F: FnOnce(T) -> T {
     // Read the value, pass it to prod, then write-over src.
-    *src = prod(unsafe { ptr::read_and_zero(src as *mut T) });
+    *src = prod(ptr::read_and_zero(src as *mut T));
 }
 
 #[test] fn test_works() {
     let mut a = 7;
     let b = &mut a;
 
-    replace_map(b, |x: usize| x * 2);
+    unsafe { replace_map(b, |x: usize| x * 2) };
     assert_eq!(*b, 14);
 }
 
-#[test] fn is_panic_safe() {
+#[test] fn is_partially_panic_safe() {
     static mut DROP_COUNT: usize = 0;
     struct Dropper;
 
@@ -43,7 +68,7 @@ where F: FnOnce(T) -> T {
         let mut a = Dropper;
         let b = &mut a;
 
-        replace_map(b, |_| panic!("Muahaha"));
+        unsafe { replace_map(b, |_| panic!("Muahaha")); }
     }).join().unwrap_err();
 
     assert_eq!(unsafe { DROP_COUNT }, 1);
